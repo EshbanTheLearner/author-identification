076bf104443a3548a0eb5b0ff4c886c9f8669dd2
Eshban Suleman <eshban9492@gmail.com>
2023-03-29 12:30:43 +0500

    Added Imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
+
+use std::io;
+use std::vec::Vec;
+use std::error::Error;
+use std::io::Write;
+use std::fs::File;
+
+use csv;
+use rand;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
+use fasttext::{ FastText, Args, ModelName, LossName };
+use stopwords;
+use std::collections::HashSet;
+use stopwords::{ Spark, Language, Stopwords };
+use itertools::Itertools;
+use vtext::tokenize::VTextTokenizer;
+use rust_stemmer::{ Algorithm, Stemmer };
+
