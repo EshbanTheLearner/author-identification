6ad9b286dd58e9f44d10f79d6e6aa1f803869e09
Eshban Suleman <eshban9492@gmail.com>
2023-03-29 12:51:54 +0500

    Added dataset struct and impl

--- src/main.rs
+++ src/main.rs
@@ -22,3 +22,35 @@ use itertools::Itertools;
 use vtext::tokenize::VTextTokenizer;
 use rust_stemmer::{ Algorithm, Stemmer };
 
+#[derive(Debug, Deserialize)]
+pub struct SpookyAuthor {
+    id: String,
+    text: String,
+    author: String
+}
+
+impl SpookyAuthor {
+    pub fn into_tokens(&self) -> String {
+        let lc_text = self.text.to_lowercase();
+        let tok = VTextTokenizer::new("en");
+        let tokens: Vec<&str> = tok.tokenize(lc_text.as_str()).collect();
+        let en_stemmer = Stemmer::create(Algorithm::English);
+        
+        let tokens: Vec<String> = tokens.iter()
+            .map(|x| en_stemmer.stem(x).into_owned())
+            .collect();
+        
+        let mut tokens: Vec<&str> = tokens.iter()
+            .map(|x| x.as_str())
+            .collect();
+        
+        let stops: HashSet<_> = Spark::stopwords(Language::English)
+            .unwrap()
+            .iter()
+            .collect();
+        
+        tokens.retain(|s| !stops.contains(s));
+
+        tokens.iter().join(" ")
+    }
+}
